cf15edf0b9fc485631591893b88a77778e0c0994
mosesvk <kaumatule93@gmail.com>
2022-04-13 10:46:44 -0600

    initial commit, install tailwindcss

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
+import Head from 'next/head';
 
 
 export default function Home() {
   return (
-    <div >
+    <div>
+      <Head>
 
+      </Head>
+      <main>
+        
+      </main>
     </div>
   )
 }
